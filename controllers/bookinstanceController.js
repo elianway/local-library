5b69a9224131e9a731d64031470c599e71827357
Eli Anway <eli.anway@gmail.com>
2021-12-21 11:54:49 -0500

    Bookinstance detail

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -14,8 +14,23 @@ exports.bookinstance_list = function(req, res, next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+    prisma.bookinstance.findUnique({
+        where: {
+            id: req.params.id,
+        },
+        include: {
+            book: true,
+        },
+    }, function(err, bookinstance) {
+        if (err) { return next(err); }
+        if (bookinstance==null) {
+            var err = new Error('Book copy not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance: bookinstance});
+    })
 };
 
 // Display BookInstance create form on GET.
