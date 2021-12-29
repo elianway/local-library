0a64fbf32143d4634ea6b16b61f0645d534a2477
Eli Anway <eli.anway@gmail.com>
2021-12-29 11:35:54 -0500

    Add book update form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -99,7 +99,7 @@ exports.book_create_get = function(req, res, next) {
 exports.book_create_post = [
     // Convert the genre to an array.
     (req, res, next) => {
-        if (!(req.body.genre instanceOf Array)) {
+        if (!(req.body.genre.instanceOf(Array))) {
             if (typeof req.body.genre === 'undefined')
                 req.body.genre = [];
                 else
@@ -170,7 +170,7 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+
 };
 
 // Handle book delete on POST.
@@ -179,11 +179,122 @@ exports.book_delete_post = function(req, res) {
 };
 
 // Display book update form on GET.
-exports.book_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update GET');
+exports.book_update_get = function(req, res, next) {
+
+    // Get book, authors and genres for form.
+    async.parallel({
+        book: function(callback) {
+            prisma.book.findUnique({
+                where: {
+                    id: req.params.id
+                },
+                include: {
+                    author: true,
+                    genre: true,
+                },
+            }, callback)
+        },
+        authors: function(callback) {
+            prisma.author.findMany({}, callback)
+        },
+        genres: function(callback) {
+            prisma.genre.findMany({}, callback)
+        },
+    }, function(e, results) {
+        if (e) { return next(e); }
+        if (results.book==null) { // No results.
+            const e = new Error('Book not found');
+            e.status = 404;
+            return next(e);
+        }
+        // Success
+        // Mark our selected genres as checked.
+        for (let all_g_iter = 0; all_g_iter < results.genres.length; all_g_iter++) {
+            for (let book_g_iter = 0; book_g_iter < results.book.genre.length; book_g_iter++) {
+                if (results.genres[all_g_iter].id.toString()===results.book.genre[book_g_iter].id.toString()) {
+                    results.genres[all_g_iter].checked='true';
+                }
+            }
+        }
+        res.render('book_form', { title: 'Update Book', authors: results.authors, genres: results.genres, book: results.book });
+    });
 };
 
 // Handle book update on POST.
-exports.book_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update POST');
-};
+exports.book_update_post = [
+
+    // Convert the genre to an array
+    (req, res, next) => {
+        if (!(req.body.genre.instanceOf(Array))) {
+            if (typeof req.body.genre==='undefined')
+            req.body.genre = [];
+            else
+            req.body.genre = new Array(req.body.genre);
+        }
+        next();
+    },
+
+    // Validate and sanitize fields
+    body('title', 'Title must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('author', 'Author must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('summary', 'Summary must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('isbn', 'ISBN must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('genre.*').escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Book object with escaped/trimmed data and old id
+        const book = {
+            title: req.body.title,
+            author: req.body.author,
+            summary: req.body.summary,
+            isbn: req.body.isbn,
+            genre: (typeof req.body.genre==='undefined') ? [] : req.body.genre,
+            id: req.params.id // This is required, or a new id will be assigned.
+        };
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with santized values/error messages.
+
+            // Get all authors and genres for form.
+            async.parallel({
+                authors: function(callback) {
+                    prisma.author.findMany({}, callback)
+                },
+                genres: function(callback) {
+                    prisma.genre.findMany({}, callback)
+                },
+            }, function(e, results) {
+                if (e) { return next(e); }
+
+                // Mark our genres as checked.
+                for (let i = 0; i < results.genres.length; i++) {
+                    if (book.genre.indexOf(results.genres[i].id) > -1) {
+                        results.genres[i].checked='true';
+                    }
+                }
+                res.render('book_form', { title: 'Update Book', authors: results.authors, genres: results.genres, book: book, errors: errors.array() });
+            });
+            return;
+        }
+        else {
+            // Data from the form is valid. Update the record.
+            try {
+                await prisma.book.update({
+                    where: {
+                        id: req.params.id
+                    },
+                    data: book,
+                })
+                res.redirect(book.id);
+            }
+            catch (e) {
+                if (e) { return next(e); }
+            }
+        }
+    }
+];
