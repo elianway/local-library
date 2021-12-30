1c53b4c48a12db8041d072e050505473b63722cd
Eli Anway <eli.anway@gmail.com>
2021-12-30 17:35:37 -0500

    Delete book instance

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -97,13 +97,34 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function(req, res, next) {
+    try {
+        const instance = await prisma.bookinstance.findUnique({
+            where: {
+                id: req.params.id
+            },
+        })
+        if (instance==null) { res.redirect('/catalog/bookinstances'); }
+        res.render('bookinstance_delete', { title: 'Delete Book Instance', book_instance: instance } );
+    }
+    catch (e) {
+        if (e) { return next(e); }
+    }
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function(req, res, next) {
+    try {
+        await prisma.bookinstance.delete({
+            where: {
+                id: req.body.instanceid
+            },
+        })
+        res.redirect('/catalog/bookinstances')
+    }
+    catch (e) {
+        if (e) { return next(e); }
+    }
 };
 
 // Display BookInstance update form on GET.
